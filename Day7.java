7cb60f9336abe913a775645c80951eedbfd16787
Honey Sonwani <honeysonwani88@gmail.com>
2019-12-18 12:37:01 +0530

    Day7 Solution 2 added

--- Day7.java
+++ Day7.java
@@ -28,3 +28,52 @@ public class Day7 {
         scanner.close();
     }
 }
+
+
+// SOLUTION 2, SWAPPING THE POSITIONS 
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Day7 {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int start = 0;
+        int end = n-1;
+        int temp;
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+        while(start < end){
+            temp = arr[start];
+            arr[start] = arr[end]; 
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+        for(int j=0; j<n ; j++){
+        System.out.print(arr[j] + " ");
+        }
+       
+
+        scanner.close();
+    }
+}
